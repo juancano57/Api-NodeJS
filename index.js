66bdd44eebc57ea58258cf01e02dc1ba8e90b8b8
Jcano <master@condorcoin.co>
2022-02-08 16:52:38 -0500

    volviendo a la normalidad

--- index.js
+++ index.js
@@ -36,38 +36,31 @@ app.get('/keypair_public_key/:mnemonic', (req, res) => {
 })
 
 
-//Crear Conexion
-function createConnection(cluster) {
-    return new web3.Connection(web3.clusterApiUrl(cluster))
-}
+// //Crear Conexion
+// function createConnection(cluster) {
+//     return new web3.Connection(web3.clusterApiUrl(cluster))
+// }
 
-// Enviar SOL
+// // Enviar SOL
+// app.get('/send_transaction/:mnemonic/:toPublicKey/:lamps', (req, res) => {
 
-app.get('/send_transaction/:mnemonic/:toPublicKey/:lamps', (req, res) => {
+//     const { mnemonic, toPublicKey, lamps } = req.params;
 
-    const { mnemonic, toPublicKey, lamps } = req.params;
-
-    const connection = createConnection("devnet")
+//     const connection = createConnection("devnet")
     
-    const seed = bip39.mnemonicToSeedSync(mnemonic, "")
-    const path = `m/44'/501'/0'/0'`;
-    const keypair = web3.Keypair.fromSeed(ed25519.derivePath(path, seed.toString("hex")).key)
+//     const seed = bip39.mnemonicToSeedSync(mnemonic, "")
+//     const path = `m/44'/501'/0'/0'`;
+//     const keypair = web3.Keypair.fromSeed(ed25519.derivePath(path, seed.toString("hex")).key)
     
-    const transferTransaction = new Transaction()
-      .add(SystemProgram.transfer({
-        fromPubkey: keypair.publicKey,
-        toPubkey: new web3.PublicKey(toPublicKey),
-        lamports: lamps / LAMPORTS_PER_SOL 
-    }))
+//     const transferTransaction = new web3.Transaction()
+//       .add(SystemProgram.transfer({
+//         fromPubkey: keypair.publicKey,
+//         toPubkey: new web3.PublicKey(toPublicKey),
+//         lamports: lamps / LAMPORTS_PER_SOL 
+//     }))
     
-    var signature = await sendAndConfirmTransaction(connection, transferTransaction, [keypair])
-
-    res.send(signature)
-})
-
-
-//Enviar SPL TOKEN
-
+//     var signature = await sendAndConfirmTransaction(connection, transferTransaction, [keypair])
+// })
 
 // Starting the Server
 app.listen(app.get('port'), () =>{
